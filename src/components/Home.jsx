2353bf27e6511dee3933998cb81c300c7d25a023
Deepak2-0 <choudharydeepak938@gmail.com>
2021-07-04 21:18:33 +0530

    home page done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import leafImage from "../leaves.jpg";
 
 const Home = () => {
 	return (
-		<div>
-			<h1>Home</h1>
-		</div>
+		<main>
+			<img
+				src={leafImage}
+				alt="Monstera Leaves"
+				className="absolute object-cover w-full h-full "
+			/>
+			<section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+				<h1 className="text-6xl text-green-100  font-bold cursive leading-none lg:leading-snug home-name">
+					Namaste. I'm Deepak.
+				</h1>
+			</section>
+		</main>
 	);
 };
 
